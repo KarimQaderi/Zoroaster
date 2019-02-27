3db6b12f7e26ffd7e8f54b4bf2c934519086f082
Karim Qaderi <32425175+KarimQaderi@users.noreply.github.com>
2019-02-27 09:28:22 +0330

    حل مشکل مخفی نکردن elemnt وقتی که صفحه باگزاری می شود

--- resources/views/fields/Form/select.blade.php
+++ resources/views/fields/Form/select.blade.php
@@ -16,22 +16,22 @@
 
 @empty(!Zoroaster::getMeta($field,'activeEelementByClass'))
     <script>
-
-        var {{ $field->name.time() }}_hiden_element = @json(Zoroaster::getMeta($field,'activeEelementByClass') );
-        activeEelementByClass({{ $field->name.time() }}_hiden_element);
-
-        $select = $('.{{ $field->name.time() }}').find(':selected').val();
-        $('.' + activeEelementByClassFind({{ $field->name.time() }}_hiden_element, $select)).removeClass('hidden');
-
         $(document).ready(function () {
-            $(document).on('change', '.{{ $field->name.time() }}', function () {
-                $select = $('.{{ $field->name.time() }}').find(':selected').val();
-                activeEelementByClass({{ $field->name.time() }}_hiden_element);
+            var {{ $field->name.time() }}_hiden_element = @json(Zoroaster::getMeta($field,'activeEelementByClass') );
+            activeEelementByClass({{ $field->name.time() }}_hiden_element);
 
-                $('.' + activeEelementByClassFind({{ $field->name.time() }}_hiden_element, $select)).removeClass('hidden');
+            $select = $('.{{ $field->name.time() }}').find(':selected').val();
+            $('.' + activeEelementByClassFind({{ $field->name.time() }}_hiden_element, $select)).removeClass('hidden');
 
+            $(document).ready(function () {
+                $(document).on('change', '.{{ $field->name.time() }}', function () {
+                    $select = $('.{{ $field->name.time() }}').find(':selected').val();
+                    activeEelementByClass({{ $field->name.time() }}_hiden_element);
+
+                    $('.' + activeEelementByClassFind({{ $field->name.time() }}_hiden_element, $select)).removeClass('hidden');
+
+                });
             });
         });
-
     </script>
 @endempty
