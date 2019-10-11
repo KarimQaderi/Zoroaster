42890cf98f0a22e9d5725fd5e74202e446a58c64
KarimQaderi <karimqaderi1@gmail.com>
2019-10-11 09:44:52 +0330

    fix search global

--- resources/views/resources/GlobalSearch.blade.php
+++ resources/views/resources/GlobalSearch.blade.php
@@ -1,5 +1,6 @@
 <label>{{ $newResource->label }}</label>
 @forelse($data as $item)
+    @php($newResource->resource = $item)
     <a class="item" href="{{ route('Zoroaster.resource.show',['resource'=> $newResource->uriKey(),'resourceId'=> $item->{$model->getKeyName()}]) }}">
         {{ $item->{$newResource->title} }}
     </a>
